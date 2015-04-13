5f6740a6dda1cdb2ec7573084d7c049c43de6c21
Siarhei Khalipski <welaribo@tut.by>
2015-04-13 11:08:20 +0300

    Added draft IRepository and draft Repository for EF implementation

---
+++ Sources/Vilandagro.Infrastructure.EF/Repository.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vilandagro.Infrastructure.EF
+{
+    public class Repository : IRepository
+    {
+        private DbContext _dbContext;
+
+        public Repository(DbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        protected DbContext DbContext
+        {
+            get { return _dbContext; }
+        }
+
+        public IEnumerable<T> GetAll<T>() where T : class
+        {
+            // TODO: It is the same as GetAll/Find... Is it OK?
+            return DbContext.Set<T>().AsEnumerable();
+        }
+
+        public IEnumerable<T> GetAll<T>(Expression<Func<T, bool>> predicate) where T : class
+        {
+            return DbContext.Set<T>().Where(predicate).AsEnumerable();
+        }
+
+        public IQueryable<T> Where<T>(Expression<Func<T, bool>> predicate) where T : class
+        {
+            return DbContext.Set<T>().Where(predicate).AsQueryable();
+        }
+
+        public IEnumerable<T> Find<T>(Expression<Func<T, bool>> predicate) where T : class
+        {
+            // TODO: It is the same as GetAll/Find... Is it OK?
+            return DbContext.Set<T>().Where(predicate).AsEnumerable();
+        }
+
+        public bool Any<T>(Expression<Func<T, bool>> predicate) where T : class
+        {
+            return DbContext.Set<T>().Any(predicate);
+        }
+
+        public T First<T>(Expression<Func<T, bool>> predicate) where T : class
+        {
+            return DbContext.Set<T>().Where(predicate).First();
+        }
+
+        public T FirstOrDefault<T>(Expression<Func<T, bool>> predicate) where T : class
+        {
+            return DbContext.Set<T>().Where(predicate).FirstOrDefault();
+        }
+
+        public void Add<T>(T entity) where T : class
+        {
+            DbContext.Set<T>().Add(entity);
+        }
+
+        public void AddRange<T>(T[] entities) where T : class
+        {
+            DbContext.Set<T>().AddRange(entities);
+        }
+
+        public void Delete<T>(T entity) where T : class
+        {
+            DbContext.Set<T>().Remove(entity);
+        }
+
+        public void DeleteRange<T>(T[] entities) where T : class
+        {
+            DbContext.Set<T>().RemoveRange(entities);
+        }
+
+        public void Attach<T>(T entity) where T : class
+        {
+            DbContext.Set<T>().Attach(entity);
+        }
+    }
+}
---
+++ Sources/Vilandagro.Infrastructure/IRepository.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace Vilandagro.Infrastructure
+{
+    public interface IRepository
+    {
+        /// <summary>
+        /// Gets all entities
+        /// </summary>        
+        /// <returns>All entities</returns>
+        IEnumerable<T> GetAll<T>() where T : class;
+
+        /// <summary>
+        /// Gets all entities matching the predicate
+        /// </summary>
+        /// <param name="predicate">The filter clause</param>
+        /// <returns>All entities matching the predicate</returns>
+        IEnumerable<T> GetAll<T>(Expression<Func<T, bool>> predicate) where T : class;
+
+        /// <summary>
+        /// Set based on where condition
+        /// </summary>
+        /// <param name="predicate">The predicate</param>
+        /// <returns>The records matching the given condition</returns>
+        IQueryable<T> Where<T>(Expression<Func<T, bool>> predicate) where T : class;
+
+        /// <summary>
+        /// Finds an entity matching the predicate
+        /// </summary>
+        /// <param name="predicate">The filter clause</param>
+        /// <returns>An entity matching the predicate</returns>
+        IEnumerable<T> Find<T>(Expression<Func<T, bool>> predicate) where T : class;
+
+        /// <summary>
+        /// Determines if there are any entities matching the predicate
+        /// </summary>
+        /// <param name="predicate">The filter clause</param>
+        /// <returns>True if a match was found</returns>
+        bool Any<T>(Expression<Func<T, bool>> predicate) where T : class;
+
+        /// <summary>
+        /// Returns the first entity that matches the predicate
+        /// </summary>
+        /// <param name="predicate">The filter clause</param>
+        /// <returns>An entity matching the predicate</returns>
+        T First<T>(Expression<Func<T, bool>> predicate) where T : class;
+
+        /// <summary>
+        /// Returns the first entity that matches the predicate else null
+        /// </summary>
+        /// <param name="predicate">The filter clause</param>
+        /// <returns>An entity matching the predicate else null</returns>
+        T FirstOrDefault<T>(Expression<Func<T, bool>> predicate) where T : class;
+
+        /// <summary>
+        /// Adds a given entity to the context
+        /// </summary>
+        /// <param name="entity">The entity to add to the context</param>
+        void Add<T>(T entity) where T : class;
+
+        /// <summary>
+        /// Adds a given entities set to the context
+        /// </summary>
+        /// <param name="entities">The entities set to add to the context</param>
+        void AddRange<T>(T[] entities) where T : class;
+
+        /// <summary>
+        /// Deletes a given entity from the context
+        /// </summary>
+        /// <param name="entity">The entity to delete</param>
+        void Delete<T>(T entity) where T : class;
+
+        /// <summary>
+        /// Deletes a given entity from the context
+        /// </summary>
+        /// <param name="entities">The entities set to delete</param>
+        void DeleteRange<T>(T[] entities) where T : class;
+
+        /// <summary>
+        /// Attaches a given entity to the context
+        /// </summary>
+        /// <param name="entity">The entity to attach</param>
+        void Attach<T>(T entity) where T : class;
+    }
+}
